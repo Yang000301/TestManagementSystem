4388599cab2e25f4d5915f7258a2e1ff23557427
阿揚啦 <toto7057@gmail.com>
2026-08-08 20:36:55 +0800

    feat(project): add project input validation

--- TestManagementSystem.API/TestManagementSystem.API/DTOs/Project/CreateProjectDto.cs
+++ TestManagementSystem.API/TestManagementSystem.API/DTOs/Project/CreateProjectDto.cs
@@ -1,9 +1,15 @@
-﻿namespace TestManagementSystem.API.DTOs.Project
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestManagementSystem.API.DTOs.Project
 {
     //創project
     public class CreateProjectDto
     {
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+
+        [StringLength(500)]
+        public string? Description { get; set; }
     }
 }
